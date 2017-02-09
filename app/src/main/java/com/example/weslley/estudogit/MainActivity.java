f6749ff00add799baafc741272dab7420dfaaebc
Weslley <weslley.cvc@hotmil.com>
2017-02-08 22:19:42 -0300

    Teste Git Comentario

--- app/src/main/java/com/example/weslley/estudogit/MainActivity.java
+++ app/src/main/java/com/example/weslley/estudogit/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Teste Git
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
